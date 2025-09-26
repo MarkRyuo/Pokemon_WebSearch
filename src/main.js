6b76a11412a7dbe7c42fc8e13cd458b57d9722e0
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-09-26 18:04:28 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -3,29 +3,34 @@ import './style.css'
 
 export function Homepage() {
   document.querySelector('#app').innerHTML = `
-  <div class="bg-blue-400 h-screen grid">
-    <nav>
-      <a href="/PokemonList" data-navigo>Pokemon</a>
-      <img src="">
-    </nav>
-
-    <main>
+  <div class=" bg-blue-400">
+    <div class="h-screen grid max-w-7xl mx-auto border">
       <div>
-        <img src="">
+        <nav class="border-red-800 flex justify-end bg-white p-5 rounded-full">
+          <a href="/PokemonList" data-navigo>Pokemon</a>
+          <img src="">
+        </nav>
       </div>
-      <div class="grid justify-center content-center gap-y-3">
-        <input type="text" name="PokemonNameInput" placeholder="Enter Pokemon Name" spellcheck required id="inptPokemon" class="w-sm p-2 bg-white rounded-sm">
-        <button type="button" id="btnSearch" class="bg-yellow-300 text-black p-2 w-40 justify-self-center rounded-sm">
-          Search
-        </button>
+
+      <main class="">
         <div>
           <img src="">
         </div>
-      </div>
-    
-    </main>
-    
+        <div class="grid justify-center content-center gap-y-3">
+          <input type="text" name="PokemonNameInput" placeholder="Enter Pokemon Name" spellcheck required id="inptPokemon" class="w-sm p-2 bg-white rounded-sm">
+          <button type="button" id="btnSearch" class="bg-yellow-300 text-black p-2 w-30 justify-self-center rounded-sm">
+            Search
+          </button>
+          <div>
+            <img src="">
+          </div>
+        </div>
+      
+      </main>
+      
+    </div>
   </div>
+
 `
 const btnSearch = document.getElementById("btnSearch") ;
 btnSearch.addEventListener("click", () => {
