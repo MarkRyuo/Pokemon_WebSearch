c605fd62c9913ae1c60b9b80e3b39da2e51b0b19
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-09-28 07:21:47 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ export function Homepage() {
   <div class=" bg-[#198AD8] h-screen overflow-hidden">
     <div class="h-screen grid max-w-7xl mx-auto border grid-rows-[1fr_2fr_1fr] gap-3.5">
       <div>
-        <nav class="border-red-800 flex justify-end bg-white/80 p-3 rounded-full items-center gap-9 max-h-15">
+        <nav class="border-red-800 flex justify-end bg-white p-3 rounded-full items-center gap-9 max-h-15">
           <a href="/PokemonList" data-navigo>Pokemon</a>
           <a href="https://www.flaticon.com/free-icon/pokeball_287221" alt="flaticon" target="_blank" title="Pokeball by Flaticon">
             <img src="${pokeball}" class="max-w-10">
