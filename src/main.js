5d3a9659806db71e760dccbd3228c6a2c76c02db
MarkRyuo <malupajhonmark3035@gmail.com>
2025-10-02 20:16:25 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './style.css'
 export function Homepage() {
   document.querySelector('#app').innerHTML = `
   <div class="bg-blue-500 w-screen h-screen overflow-hidden box-border">
-    <div class="grid grid-rows-[1fr, 1fr,] h-screen">
+    <div class="grid grid-rows-[1fr, 1fr,] h-screen w-full max-w-7xl m-auto">
       <div class="w-screen h-20">
         <nav class="bg-white rounded-full flex flex-row justify-end items-center gap-1 p-2 w-full">
           <a href="/PokemonList" data-navigo>Pokemon</a>
