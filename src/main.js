ea048c0f9008aae84ff77d81a40a9b18cb46c1d1
MarkRyuo <malupajhonmark3035@gmail.com>
2025-10-02 20:07:37 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -6,20 +6,20 @@ import './style.css'
 
 export function Homepage() {
   document.querySelector('#app').innerHTML = `
-  <div class="">
-    <div class="">
-      <div>
-        <nav class="">
+  <div class="bg-blue-500 w-screen h-screen overflow-hidden box-border">
+    <div class="grid grid-rows-[1fr, 1fr,] h-screen m-auto">
+      <div class="w-screen h-20">
+        <nav class="bg-white rounded-full flex flex-row justify-end items-center gap-1 p-2 w-full">
           <a href="/PokemonList" data-navigo>Pokemon</a>
           <a href="https://www.flaticon.com/free-icon/pokeball_287221" alt="flaticon" target="_blank" title="Pokeball by Flaticon">
-            <img src="${pokeball}" class="">
+            <img src="${pokeball}" class="w-full max-w-8 min-w-4 ">
           </a>
-        </nav>j
+        </nav>
       </div>
 
-      <main class="">
+      <main class="w-screen relative flex jus">
         <div class="">
-          <img src="${pokemontext}" class="">
+          <img src="${pokemontext}" class="w-full max-w-md min-w-2xs">
         </div>
         <div class="">
           <input type="text" name="PokemonNameInput" placeholder="Enter Pokemon Name" spellcheck required id="inptPokemon" class="w-sm p-2 bg-white rounded-xl inset-shadow-amber-50">
@@ -29,8 +29,8 @@ export function Homepage() {
         </div>
           
       </main>
-      <div class="">
-        <img src="${pikachu}" class="">
+      <div class="absolute">
+        <img src="${pikachu}" class="hidden">
       </div>
       
     </div>
