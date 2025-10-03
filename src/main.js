50e6dd7f73a6a9b92fd9131189e40486988295ca
MarkRyuo <malupajhonmark3035@gmail.com>
2025-10-03 10:32:48 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ export function Homepage() {
   <div class="bg-blue-500 w-screen h-screen overflow-hidden box-border">
     <div class="grid grid-rows-[80px, 1fr] h-screen sm:max-w-7xl sm:m-auto">
       <div class="w-screen sm:w-full">
-        <nav class="bg-white rounded-full flex flex-row justify-end items-center gap-1 p-2 w-full">
-          <a href="/PokemonList" data-navigo>Pokemon</a>
+        <nav class="bg-white/75 rounded-full flex flex-row justify-end items-center gap-3 p-2 w-full">
+          <a href="/PokemonList" data-navigo>Pokédex</a>
           <a href="https://www.flaticon.com/free-icon/pokeball_287221" alt="flaticon" target="_blank" title="Pokeball by Flaticon">
             <img src="${pokeball}" class="w-full max-w-8 min-w-4 ">
           </a>
@@ -19,11 +19,11 @@ export function Homepage() {
 
       <main class="relative flex flex-col items-center">
         <div class="">
-          <img src="${pokemontext}" class="w-full max-w-md min-w-2xs">
+          <img src="${pokemontext}" class="w-full max-w-xl min-w-2xs">
         </div>
-        <div class="flex flex-col">
-          <input type="text" name="PokemonNameInput" placeholder="Enter Pokemon Name" spellcheck required id="inptPokemon" class="w-screen sm:w-sm p-2 bg-white rounded-xl inset-shadow-amber-50">
-          <button type="button" id="btnSearch" class="">
+        <div class="flex flex-col gap-2 items-center">
+          <input type="text" name="PokemonNameInput" placeholder="Enter Pokemon Name" spellcheck required id="inptPokemon" class="w-screen sm:w-sm p-2.5 bg-white rounded-xl inset-shadow-amber-50">
+          <button type="button" id="btnSearch" class="bg-yellow-300 w-full max-w-28 p-1.5 rounded-xl font-bold text-base">
             Search
           </button>
         </div>
